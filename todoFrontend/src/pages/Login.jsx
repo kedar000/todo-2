036873b1a801b80808f3bd2f8a7739cd80f65181
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-06-22 08:21:51 +0530

    ts login

--- todoFrontend/src/pages/Login.jsx
+++ todoFrontend/src/pages/Login.jsx
@@ -32,14 +32,32 @@ export function Login(){
     return (
         <>
         <Navbar />
-            <div>
-                <div className="flex flex-col ">
-                    <input onChange={e =>{ setsignInMail(e.target.value)     }} type="text" placeholder="Enter The Email"></input>
-                    <input onChange={e =>{ setSignInPassword(e.target.value) }} type="text" placeholder="Enter The Password"></input>
-                </div>
-                <div className="flex flex-col justify-between ">
-                    <button onClick={userSignIn}>Login</button>
-                    <p>Create a New Account <button onClick={signup}>SignUp</button></p>
+            <div className="bg-color-offwhite flex items-center justify-center h-screen">
+                <div className="w-full max-w-sm mx-auto bg-color-lightgreen p-8 rounded-lg shadow-md">
+                    <h2 class="text-2xl font-bold mb-6 text-center ">Login</h2>
+                    <div className="flex flex-col ">
+                        <p className=" text-sm font-semibold mb-2 ">Email</p>
+                        <input onChange={e =>{ setsignInMail(e.target.value)     }} 
+                            type="text" 
+                            placeholder="Enter The Email"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        ></input>
+                        <p className=" text-sm font-semibold mb-2 mt-6">Password</p>
+                        <input onChange={e =>{ setSignInPassword(e.target.value) }} 
+                            type="password" 
+                            placeholder="Enter The Password"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        ></input>
+                    </div>
+                    <div className="flex flex-col justify-between ">
+
+                        <button onClick={userSignIn}
+                        className=" w-full bg-color-reddish my-5 p-2 rounded-md text-color-offwhite text-l font-semibold"
+                        >Login</button>
+
+                        <p className=" w-full items-center justify-center px-3"
+                        >Create a New Account ? <button onClick={signup} className="align-baseline ">SignUp</button></p>
+                    </div>
                 </div>
             </div>
         </>
